2439e19a9f88acbacc17fe9a82448caba6637c12
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-10 16:33:00 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
+  end
+
+  def show # students#show controller will search for a file of show.html.erb to RENDER this method 
+    @student = Student.find(params[:id])
+    # router sends the requested information to match with the params of '/students/:id' if if fact is what the user is requesting
   end
 end
